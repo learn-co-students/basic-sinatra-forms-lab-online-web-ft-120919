448c3b08c10e8c6d8cdb62a697b206970b2bd71c
nwscala <nwscala@gmail.com>
2020-02-04 10:59:45 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+    get '/newteam' do
+        erb :newteam
+    end
 
-
+    post '/team' do
+        @params_hash = params
+        
+        erb :team
+    end
 end
