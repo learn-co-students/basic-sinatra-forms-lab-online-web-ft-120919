0c13810d5b0bd010a5eb88ef65b16221065dc5d4
mrarthurwhite <mister.arthur.white@gmail.com>
2020-02-06 09:33:59 -0500

    Done.

--- spec/basic_sinatra_forms_spec.rb
+++ spec/basic_sinatra_forms_spec.rb
@@ -2,7 +2,7 @@ describe App do
 
   describe 'GET /newteam' do
     it 'sends a 200 status code' do
-      get '/newteam'
+        get '/newteam'
       expect(last_response.status).to eq(200)
     end
 
