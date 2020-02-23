1dc90a1bfd774f21006241d1932347001757d212
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-02-23 18:33:21 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,19 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+get '/newteam' do
+    erb :newteam
+end
 
+  post '/team' do
+    @name = params["name"]
+    @coach = params["coach"]
+    @Point Guard = params["point guard"]
+    @Shooting Guard = params["shooting guard"]
+    @Small Forward = params["small forward"]
+    @Power Forward = params["power forward"]
+    @center = params["center"]
+
+    erb :team
+  end
 end
